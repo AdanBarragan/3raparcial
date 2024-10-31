95762a74bc3643f825e79b5de843ec3a4fc5c92c
AdanBarragan <abarragan14@ucol.mx>
2024-10-31 10:11:22 -0600

    Subiendo actividad del 3er parcial

---
+++ lib/pages/people.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class People extends StatelessWidget {
+  const People({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        'Welcome to People view',
+        style: TextStyle(
+            fontSize: 22.0, fontWeight: FontWeight.bold, color: Colors.purple),
+      ),
+    );
+  }
+}
